abd36ad48502f621283f488b4f6b4cf8999d33bb
Mouhamed Naby Ndiaye <50000511+Nabzozifo@users.noreply.github.com>
2020-06-29 23:38:30 +0100

    Create awale.py

---
+++ awale.py
@@ -0,0 +1,10 @@
+class Awale():
+	def __init__(self,num_graine=48,plateau={"Nord":[4 for i in range (6)],"Sud":[4 for i in range (6)]}):
+		self.num_graine=num_graine
+		self.plateau=plateau
+
+	
+'''aw=Awale()
+print(aw.plateau)
+print(aw.plateau["Nord"])
+print(sum(aw.plateau["Nord"]))'''
